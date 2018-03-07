82eb4a2c09599cb8d91cc85cbebf331834517b2e
SUTTROOGUN Yogin Kumar <yo.suttroogun@gmail.com>
2018-03-07 19:07:13 +0400

    init project

---
+++ O'WishTests/O_WishTests.swift
@@ -0,0 +1,36 @@
+//
+//  O_WishTests.swift
+//  O'WishTests
+//
+//  Created by SUTTROOGUN Yogin Kumar on 07/03/2018.
+//  Copyright © 2018 SUTTROOGUN Yogin Kumar. All rights reserved.
+//
+
+import XCTest
+@testable import O_Wish
+
+class O_WishTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
