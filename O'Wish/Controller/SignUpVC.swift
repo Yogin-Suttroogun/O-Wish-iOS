8b5898fe4b86204acaffa2276ade957c07ef13b8
SUTTROOGUN Yogin Kumar <yo.suttroogun@gmail.com>
2018-03-10 16:03:54 +0400

    Sign Up UI

--- O'Wish/Controller/SignUpVC.swift
+++ O'Wish/Controller/SignUpVC.swift
@@ -1,17 +1,17 @@
 //
-//  ViewController.swift
+//  SignUpVC.swift
 //  O'Wish
 //
-//  Created by SUTTROOGUN Yogin Kumar on 07/03/2018.
+//  Created by SUTTROOGUN Yogin Kumar on 10/03/2018.
 //  Copyright © 2018 SUTTROOGUN Yogin Kumar. All rights reserved.
 //
 
 import UIKit
 
-class SignInVC: UIViewController {
+class SignUpVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-}
 
+}
