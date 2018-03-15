fcff3219c4e65894b99496e8c29e69fcb9292b69
SUTTROOGUN Yogin Kumar <yo.suttroogun@gmail.com>
2018-03-15 04:59:25 +0400

    Database and structuring code

---
+++ OWish/Resources/Validator.swift
@@ -0,0 +1,13 @@
+//
+//  Validator.swift
+//  OWish
+//
+//  Created by SUTTROOGUN Yogin Kumar on 15/03/2018.
+//  Copyright © 2018 SUTTROOGUN Yogin Kumar. All rights reserved.
+//
+
+import UIKit
+
+class Validator: NSObject {
+
+}
