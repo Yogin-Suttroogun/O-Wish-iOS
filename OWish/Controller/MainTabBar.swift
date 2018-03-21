a987b583284676d4bbc212adad96feb2074a4967
SUTTROOGUN Yogin Kumar <yo.suttroogun@gmail.com>
2018-03-21 19:02:26 +0400

    Log out

--- OWish/Controller/MainTabBar.swift
+++ OWish/Controller/MainTabBar.swift
@@ -12,7 +12,15 @@ class MainTabBar: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.navigationItem.setHidesBackButton(true, animated: true)
+        let logOutBtn:UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.reply, target: self, action: #selector(AdminVC.back))
+        self.navigationItem.leftBarButtonItem = logOutBtn
+        self.navigationItem.leftBarButtonItem?.tintColor = UIColor(displayP3Red: 200/255.0, green: 5/255.0, blue: 5/255.0, alpha: 0.5)
+    }
+    
+    func back(sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "logOutUser", sender: nil)
     }
 
 }
